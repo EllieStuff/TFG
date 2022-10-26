55891fb5ba61bd217e382eff0b95bfda884bd228
xavi <30593493+xdman21@users.noreply.github.com>
2022-10-26 16:25:17 +0200

    new tiny fix to the player life system

--- TFG/Assets/scripts/Player/PlayerLifeSystem.cs
+++ TFG/Assets/scripts/Player/PlayerLifeSystem.cs
@@ -10,4 +10,17 @@ public class PlayerLifeSystem : MonoBehaviour
     private enum playerStates { NORMAL }
 
     [SerializeField] private playerStates playerState;
+
+    private void Update()
+    {
+        CheckPlayerLifeLimits();
+    }
+    void CheckPlayerLifeLimits()
+    {
+        if (life > playerMaxLife)
+            life = playerMaxLife;
+        else if (life < 0)
+            life = 0;
+    }
+
 }
