3ceb893909d679264c141fff2edc5db56bb33a1c
xavi <30593493+xdman21@users.noreply.github.com>
2022-12-01 19:44:59 +0100

    restored the old cacorato enemy sword system joined with the new cacorato

---
+++ TFG/Assets/scripts/Enemies/Enemy_CacoRato_Melee.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_CacoRato_Melee : BaseEnemyScript
+{
+    //[Header("CacoRato")]
+    [SerializeField] float attackDistance;
+    [SerializeField] float baseAttackTimer;
+    [SerializeField] float attackAnimationTime;
+    [SerializeField] float attackDamage;
+    [SerializeField] EnemyWeaponHand handWeapon;
+    [SerializeField] Animator enemyAnimator;
+    [SerializeField] TrailRenderer trailsEffect;
+
+    float attackTimer;
+
+    bool attacking;
+
+    LifeSystem playerLife;
+
+    [SerializeField] Animation swordAnim;
+
+    internal override void Start_Call() { base.Start_Call(); playerLife = player.GetComponent<LifeSystem>(); }
+
+    internal override void Update_Call() { base.Update_Call(); }
+
+    internal override void FixedUpdate_Call() { base.FixedUpdate_Call(); }
+
+
+    internal override void IdleUpdate()
+    {
+        enemyAnimator.SetFloat("state", 0);
+        base.IdleUpdate();
+    }
+    internal override void MoveToTargetUpdate()
+    {
+        enemyAnimator.SetFloat("state", 1);
+        base.MoveToTargetUpdate();
+
+    }
+    internal override void AttackUpdate()
+    {
+        base.AttackUpdate();
+
+        if (attacking && handWeapon.isTouchingPlayer)
+            playerLife.Damage(attackDamage, playerLife.healthState);
+
+        if (Vector3.Distance(player.position, transform.position) > attackDistance)
+        {
+            enemyAnimator.SetFloat("state", 1);
+            Vector3 targetMoveDir = (player.position - transform.position).normalized;
+            MoveRB(targetMoveDir, actualMoveSpeed * speedMultiplier);
+        }
+        else
+        {
+            enemyAnimator.SetFloat("state", 0);
+
+            rb.velocity = Vector3.zero;
+
+            attackTimer -= Time.deltaTime;
+
+            if(attackTimer <= 0)
+            {
+                StartCoroutine(AttackCorroutine());
+                attackTimer = baseAttackTimer;
+            }
+        }
+    }
+
+    IEnumerator AttackCorroutine()
+    {
+        swordAnim.Play();
+
+        trailsEffect.enabled = true;
+        attacking = true;
+
+        yield return new WaitForSeconds(attackAnimationTime);
+
+        attacking = false;
+        trailsEffect.enabled = false;
+    }
+
+
+    internal override void IdleStart() { base.IdleStart(); }
+    internal override void MoveToTargetStart() { base.MoveToTargetStart(); }
+    internal override void AttackStart() { base.AttackStart(); }
+
+
+    internal override void IdleExit() { base.IdleExit(); }
+    internal override void MoveToTargetExit() { base.MoveToTargetExit(); }
+    internal override void AttackExit() { base.AttackExit(); }
+
+
+}
