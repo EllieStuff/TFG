9f9e4e0334cbadf52a9552108107d5a2567be57a
xavifm <60983549+xavifm@users.noreply.github.com>
2023-02-10 12:44:00 +0100

    modified the particles quantity while the player is not moving

--- TFG/Assets/scripts/Misc/ParticleScript.cs
+++ TFG/Assets/scripts/Misc/ParticleScript.cs
@@ -10,7 +10,14 @@ public class ParticleScript : MonoBehaviour
     [SerializeField] float maxTime;
     [SerializeField] bool removeParent;
     [SerializeField] bool followPlayer;
+    [SerializeField] bool permanent;
+    [SerializeField] bool playerParticle;
+    [SerializeField] Rigidbody playerRB;
+
+    int originalMaxParticles;
     float destroyTimer;
+
+    ParticleSystem particles;
 
     private void Start()
     {
@@ -23,12 +30,30 @@ public class ParticleScript : MonoBehaviour
             destroyTimer = timeToDestroy;
         else
             destroyTimer = Random.Range(minTime, maxTime);
+
+        if (playerParticle)
+        {
+            particles = GetComponent<ParticleSystem>();
+            var main = particles.main;
+            originalMaxParticles = main.maxParticles;
+        }
     }
 
     private void Update()
     {
-        destroyTimer -= Time.deltaTime;
-        if (destroyTimer <= 0)
-            Destroy(gameObject);
+        if(!permanent)
+        {
+            destroyTimer -= Time.deltaTime;
+            if (destroyTimer <= 0)
+                Destroy(gameObject);
+        }
+        if(playerParticle)
+        {
+            var main = particles.main;
+            if (playerRB.velocity.magnitude <= 1)
+                main.maxParticles = originalMaxParticles / 100;
+            else
+                main.maxParticles = originalMaxParticles;
+        }
     }
 }
