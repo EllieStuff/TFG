5c3cc477c8d6d16e8fc2fd29d5c510938bdd28cd
xavifm <60983549+xavifm@users.noreply.github.com>
2023-03-07 20:26:08 +0100

    added a new select pasive skill circle close animation

--- TFG/Assets/scripts/Map/OpenCardSelectMenu.cs
+++ TFG/Assets/scripts/Map/OpenCardSelectMenu.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class OpenCardSelectMenu : MonoBehaviour
 {
     [SerializeField] GameObject pasiveSkillUI;
+    [SerializeField] ParticleSystem infiniteParticle;
     bool opened = false;
     bool destroying = false;
 
-    const float ANIMATION_SPEED = 0.5f;
+    const float ANIMATION_SPEED = 2f;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -23,24 +24,33 @@ public class OpenCardSelectMenu : MonoBehaviour
     {
         if(pasiveSkillUI == null)
             pasiveSkillUI = GameObject.FindGameObjectWithTag("EnemyManager").transform.GetChild(0).GetComponent<RoomEnemyManager>().elementChoose;
-        
-        if(opened && !destroying && !pasiveSkillUI.activeSelf)
-        {
-            StartCoroutine(CloseCircleCorroutine());
-            destroying = true;
-        }
+
+        DestroyCircleAnimation();
     }
 
-    IEnumerator CloseCircleCorroutine()
+    void StopParticles()
     {
-        while(transform.localScale.x > 1)
+        GetComponent<ParticleSystem>().Stop();
+
+        foreach (Transform child in transform)
+            child.GetComponent<ParticleSystem>().Stop();
+
+        infiniteParticle.Play();
+    }
+
+    void DestroyCircleAnimation()
+    {
+        if (opened && !destroying && !pasiveSkillUI.activeSelf)
+        {
+            destroying = true;
+            StopParticles();
+        }
+
+        if (destroying && transform.localScale.x > 0)
         {
             float deltatime = Time.deltaTime;
             transform.localScale -= new Vector3(deltatime, deltatime, deltatime) * ANIMATION_SPEED;
         }
-
-        Destroy(gameObject);
-
-        yield return null;
     }
+
 }
